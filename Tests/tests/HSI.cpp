8a7e006882b67dfe0e2ab2eb7a3b27c2c301737f
tyanmahou <tyanmahou@gmail.com>
2024-05-26 17:18:59 +0900

    add HSI test

---
+++ Tests/tests/HSI.cpp
@@ -0,0 +1,26 @@
+﻿import ColorCpp;
+
+#include "catch.hpp"
+#include "TestUtil.hpp"
+using namespace colorcpp;
+
+TEST_CASE("HSI")
+{
+    SECTION("RGB to HSI")
+    {
+        constexpr RGB rgb{ 0.8, 0.5, 0.25 };
+        HSI hsi = ColorCast<HSI>(rgb);
+        REQUIRE(hsi.h == 27.0_nearly(1.0));
+        REQUIRE(hsi.s == 0.51_nearly(0.01));
+        REQUIRE(hsi.i == 0.52_nearly(0.01));
+    }
+    SECTION("HSI to RGB")
+    {
+        constexpr HSI hsi{ 27.0, 0.51, 0.52 };
+        RGB rgb = ColorCast<RGB>(hsi);
+
+        REQUIRE(rgb.r == 0.8_nearly(0.01));
+        REQUIRE(rgb.g == 0.5_nearly(0.01));
+        REQUIRE(rgb.b == 0.25_nearly(0.01));
+    }
+}
