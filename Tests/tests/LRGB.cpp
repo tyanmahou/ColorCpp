ae697096a2f119fe586b2bc8dbd39deaf2c8f757
tyanmahou <tyanmahou@gmail.com>
2024-05-28 23:12:33 +0900

    add LRGB test

---
+++ Tests/tests/LRGB.cpp
@@ -0,0 +1,18 @@
+﻿import ColorCpp;
+
+#include "catch.hpp"
+#include "TestUtil.hpp"
+using namespace colorcpp;
+
+TEST_CASE("LRGB")
+{
+    SECTION("RGB to LRGB to RGB")
+    {
+        constexpr RGB src{ 0.1, 0.2, 0.3 };
+        const RGB dst = ColorCast<RGB>(ColorCast<LRGB>(src));
+
+        REQUIRE(dst.r == 0.1_nearly);
+        REQUIRE(dst.g == 0.2_nearly);
+        REQUIRE(dst.b == 0.3_nearly);
+    }
+}
