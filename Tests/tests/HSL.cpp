42282eb2f72f005d875b4f6b4ec74ac596fe917f
tyanmahou <tyanmahou@gmail.com>
2024-05-29 21:30:40 +0900

    update hsl test

--- Tests/tests/HSL.cpp
+++ Tests/tests/HSL.cpp
@@ -6,17 +6,35 @@ using namespace colorcpp;
 
 TEST_CASE("HSL")
 {
-    SECTION("RGB to HSL")
+    SECTION("RGB to HSL Cylinder")
     {
         constexpr RGB rgb{ 0.8, 0.5, 0.25 };
-        HSL hsl = ColorCast<HSL>(rgb);
+        HSLCylinder hsl = ColorCast<HSLCylinder>(rgb);
         REQUIRE(hsl.h == 27.0_nearly(1.0));
         REQUIRE(hsl.s == 0.57_nearly(0.01));
         REQUIRE(hsl.l == 0.52_nearly(0.01));
     }
-    SECTION("HSL to RGB")
+    SECTION("HSL Cylinder to RGB")
     {
-        constexpr HSL hsl{ 27.0, 0.57, 0.52 };
+        constexpr HSLCylinder hsl{ 27.0, 0.57, 0.52 };
+        RGB rgb = ColorCast<RGB>(hsl);
+
+        REQUIRE(rgb.r == 0.8_nearly(0.01));
+        REQUIRE(rgb.g == 0.5_nearly(0.01));
+        REQUIRE(rgb.b == 0.25_nearly(0.01));
+    }
+
+    SECTION("RGB to HSL Cone")
+    {
+        constexpr RGB rgb{ 0.8, 0.5, 0.25 };
+        HSLCone hsl = ColorCast<HSLCone>(rgb);
+        REQUIRE(hsl.h == 27.0_nearly(1.0));
+        REQUIRE(hsl.s == 0.55_nearly(0.01));
+        REQUIRE(hsl.l == 0.52_nearly(0.01));
+    }
+    SECTION("HSL Cone to RGB")
+    {
+        constexpr HSLCone hsl{ 27.0, 0.55, 0.52 };
         RGB rgb = ColorCast<RGB>(hsl);
 
         REQUIRE(rgb.r == 0.8_nearly(0.01));
