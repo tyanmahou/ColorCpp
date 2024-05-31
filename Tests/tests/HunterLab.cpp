22e2ecb10ec327b690dc564043873f36846bb1d8
tyanmahou <tyanmahou@gmail.com>
2024-05-31 23:21:00 +0900

    add HunterLab

---
+++ Tests/tests/HunterLab.cpp
@@ -0,0 +1,28 @@
+﻿import ColorCpp;
+
+#include "catch.hpp"
+#include "TestUtil.hpp"
+using namespace colorcpp;
+
+TEST_CASE("HunterLab")
+{
+    SECTION("RGB to HunterLab50 to RGB")
+    {
+        constexpr RGB src{ 0.1, 0.2, 0.3 };
+        const RGB dst = ColorCast<RGB>(ColorCast<HunterLab50>(src));
+
+        REQUIRE(dst.r == 0.1_nearly);
+        REQUIRE(dst.g == 0.2_nearly);
+        REQUIRE(dst.b == 0.3_nearly);
+    }
+
+    SECTION("RGB to HunterLab65 to RGB")
+    {
+        constexpr RGB src{ 0.1, 0.2, 0.3 };
+        const RGB dst = ColorCast<RGB>(ColorCast<HunterLab65>(src));
+
+        REQUIRE(dst.r == 0.1_nearly);
+        REQUIRE(dst.g == 0.2_nearly);
+        REQUIRE(dst.b == 0.3_nearly);
+    }
+}
