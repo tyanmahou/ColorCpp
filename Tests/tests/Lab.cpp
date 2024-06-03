7e62918325ff400f3f2ba809eeed613b2cfbe7c6
tyanmahou <tyanmahou@gmail.com>
2024-06-03 20:26:05 +0900

    update Lab

---
+++ Tests/tests/Lab.cpp
@@ -0,0 +1,28 @@
+﻿import ColorCpp;
+
+#include "catch.hpp"
+#include "TestUtil.hpp"
+using namespace colorcpp;
+
+TEST_CASE("Lab")
+{
+    SECTION("RGB to Lab50 to RGB")
+    {
+        constexpr RGB src{ 0.1, 0.2, 0.3 };
+        const RGB dst = ColorCast<RGB>(ColorCast<Lab50>(src));
+
+        REQUIRE(dst.r == 0.1_nearly);
+        REQUIRE(dst.g == 0.2_nearly);
+        REQUIRE(dst.b == 0.3_nearly);
+    }
+
+    SECTION("RGB to Lab65 to RGB")
+    {
+        constexpr RGB src{ 0.1, 0.2, 0.3 };
+        const RGB dst = ColorCast<RGB>(ColorCast<Lab65>(src));
+
+        REQUIRE(dst.r == 0.1_nearly);
+        REQUIRE(dst.g == 0.2_nearly);
+        REQUIRE(dst.b == 0.3_nearly);
+    }
+}
