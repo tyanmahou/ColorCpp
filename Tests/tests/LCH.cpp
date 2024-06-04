586ef4a78e5e3a5c7309ffcf82ad08bb13c14b5d
tyanmahou <tyanmahou@gmail.com>
2024-06-04 22:38:52 +0900

    update LCH

---
+++ Tests/tests/LCH.cpp
@@ -0,0 +1,28 @@
+﻿import ColorCpp;
+
+#include "catch.hpp"
+#include "TestUtil.hpp"
+using namespace colorcpp;
+
+TEST_CASE("LCH")
+{
+    SECTION("RGB to LCH50 to RGB")
+    {
+        constexpr RGB src{ 0.1, 0.2, 0.3 };
+        const RGB dst = ColorCast<RGB>(ColorCast<LCH50>(src));
+
+        REQUIRE(dst.r == 0.1_nearly);
+        REQUIRE(dst.g == 0.2_nearly);
+        REQUIRE(dst.b == 0.3_nearly);
+    }
+
+    SECTION("RGB to LCH65 to RGB")
+    {
+        constexpr RGB src{ 0.1, 0.2, 0.3 };
+        const RGB dst = ColorCast<RGB>(ColorCast<LCH65>(src));
+
+        REQUIRE(dst.r == 0.1_nearly);
+        REQUIRE(dst.g == 0.2_nearly);
+        REQUIRE(dst.b == 0.3_nearly);
+    }
+}
