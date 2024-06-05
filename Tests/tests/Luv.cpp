b048426ca94789619db61bde5d3e186ae2935827
tyanmahou <tyanmahou@gmail.com>
2024-06-05 23:36:05 +0900

    update Luv

---
+++ Tests/tests/Luv.cpp
@@ -0,0 +1,28 @@
+﻿import ColorCpp;
+
+#include "catch.hpp"
+#include "TestUtil.hpp"
+using namespace colorcpp;
+
+TEST_CASE("Luv")
+{
+    SECTION("XYZ50 to Luv50 to XYZ50")
+    {
+        constexpr XYZ50 src{ 0.1, 0.2, 0.3 };
+        const XYZ50 dst = ColorCast<XYZ50>(ColorCast<Luv50>(src));
+
+        REQUIRE(dst.x == 0.1_nearly);
+        REQUIRE(dst.y == 0.2_nearly);
+        REQUIRE(dst.z == 0.3_nearly);
+    }
+
+    SECTION("XYZ65 to Luv65 to XYZ65")
+    {
+        constexpr XYZ65 src{ 0.1, 0.2, 0.3 };
+        const XYZ65 dst = ColorCast<XYZ65>(ColorCast<Luv65>(src));
+
+        REQUIRE(dst.x == 0.1_nearly);
+        REQUIRE(dst.y == 0.2_nearly);
+        REQUIRE(dst.z == 0.3_nearly);
+    }
+}
