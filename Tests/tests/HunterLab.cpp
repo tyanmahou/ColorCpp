9b4897d20241e274a54e0535bb71f881e7ad4fcc
tyanmahou <tyanmahou@gmail.com>
2024-06-05 23:38:35 +0900

    update HunterLab test

--- Tests/tests/HunterLab.cpp
+++ Tests/tests/HunterLab.cpp
@@ -6,23 +6,23 @@ using namespace colorcpp;
 
 TEST_CASE("HunterLab")
 {
-    SECTION("RGB to HunterLab50 to RGB")
+    SECTION("XYZ50 to HunterLab50 to XYZ50")
     {
-        constexpr RGB src{ 0.1, 0.2, 0.3 };
-        const RGB dst = ColorCast<RGB>(ColorCast<HunterLab50>(src));
+        constexpr XYZ50 src{ 0.1, 0.2, 0.3 };
+        const XYZ50 dst = ColorCast<XYZ50>(ColorCast<HunterLab50>(src));
 
-        REQUIRE(dst.r == 0.1_nearly);
-        REQUIRE(dst.g == 0.2_nearly);
-        REQUIRE(dst.b == 0.3_nearly);
+        REQUIRE(dst.x == 0.1_nearly);
+        REQUIRE(dst.y == 0.2_nearly);
+        REQUIRE(dst.z == 0.3_nearly);
     }
 
-    SECTION("RGB to HunterLab65 to RGB")
+    SECTION("XYZ65 to HunterLab65 to XYZ65")
     {
-        constexpr RGB src{ 0.1, 0.2, 0.3 };
-        const RGB dst = ColorCast<RGB>(ColorCast<HunterLab65>(src));
+        constexpr XYZ65 src{ 0.1, 0.2, 0.3 };
+        const XYZ65 dst = ColorCast<XYZ65>(ColorCast<HunterLab65>(src));
 
-        REQUIRE(dst.r == 0.1_nearly);
-        REQUIRE(dst.g == 0.2_nearly);
-        REQUIRE(dst.b == 0.3_nearly);
+        REQUIRE(dst.x == 0.1_nearly);
+        REQUIRE(dst.y == 0.2_nearly);
+        REQUIRE(dst.z == 0.3_nearly);
     }
 }
