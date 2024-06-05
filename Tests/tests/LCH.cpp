6295dbc48d20268217ff960c41fd0fdb93dc8568
tyanmahou <tyanmahou@gmail.com>
2024-06-05 23:42:01 +0900

    update LCH test

--- Tests/tests/LCH.cpp
+++ Tests/tests/LCH.cpp
@@ -6,23 +6,23 @@ using namespace colorcpp;
 
 TEST_CASE("LCH")
 {
-    SECTION("RGB to LCH50 to RGB")
+    SECTION("Lab50 to LCH50 to Lab50")
     {
-        constexpr RGB src{ 0.1, 0.2, 0.3 };
-        const RGB dst = ColorCast<RGB>(ColorCast<LCH50>(src));
+        constexpr Lab50 src{ 120, 0.2, 0.3 };
+        const Lab50 dst = ColorCast<Lab50>(ColorCast<LCH50>(src));
 
-        REQUIRE(dst.r == 0.1_nearly);
-        REQUIRE(dst.g == 0.2_nearly);
+        REQUIRE(dst.l == 120_nearly);
+        REQUIRE(dst.a == 0.2_nearly);
         REQUIRE(dst.b == 0.3_nearly);
     }
 
-    SECTION("RGB to LCH65 to RGB")
+    SECTION("Lab65 to LCH65 to Lab65")
     {
-        constexpr RGB src{ 0.1, 0.2, 0.3 };
-        const RGB dst = ColorCast<RGB>(ColorCast<LCH65>(src));
+        constexpr Lab65 src{ 120, 0.2, 0.3 };
+        const Lab65 dst = ColorCast<Lab65>(ColorCast<LCH65>(src));
 
-        REQUIRE(dst.r == 0.1_nearly);
-        REQUIRE(dst.g == 0.2_nearly);
+        REQUIRE(dst.l == 120_nearly);
+        REQUIRE(dst.a == 0.2_nearly);
         REQUIRE(dst.b == 0.3_nearly);
     }
 }
