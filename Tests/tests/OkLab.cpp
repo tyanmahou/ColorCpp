609a8c49bf47be70a768f8c2566eca87ddbef994
tyanmahou <tyanmahou@gmail.com>
2024-06-06 23:05:49 +0900

    update OkLab test

---
+++ Tests/tests/OkLab.cpp
@@ -0,0 +1,28 @@
+﻿import ColorCpp;
+
+#include "catch.hpp"
+#include "TestUtil.hpp"
+using namespace colorcpp;
+
+TEST_CASE("OkLab")
+{
+    SECTION("XYZ65 to OkLab to XYZ65")
+    {
+        constexpr XYZ65 src{ 0.1, 0.2, 0.3 };
+        const XYZ65 dst = ColorCast<XYZ65>(ColorCast<OkLab>(src));
+
+        REQUIRE(dst.x == 0.1_nearly);
+        REQUIRE(dst.y == 0.2_nearly);
+        REQUIRE(dst.z == 0.3_nearly);
+    }
+
+    SECTION("LRGB to OkLab to LRGB")
+    {
+        constexpr LRGB src{ 0.1, 0.2, 0.3 };
+        const LRGB dst = ColorCast<LRGB>(ColorCast<OkLab>(src));
+
+        REQUIRE(dst.r == 0.1_nearly);
+        REQUIRE(dst.g == 0.2_nearly);
+        REQUIRE(dst.b == 0.3_nearly);
+    }
+}
