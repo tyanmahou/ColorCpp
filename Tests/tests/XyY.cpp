6349175ee0bae15384c21c98cddee61f638e9bd7
tyanmahou <tyanmahou@gmail.com>
2024-06-09 18:39:20 +0900

    add XyY test

---
+++ Tests/tests/XyY.cpp
@@ -0,0 +1,27 @@
+﻿import ColorCpp;
+
+#include "catch.hpp"
+#include "TestUtil.hpp"
+using namespace colorcpp;
+
+TEST_CASE("XyY")
+{
+    SECTION("XYZ65 to XyY65 to XYZ65")
+    {
+        constexpr XYZ65 src{ 0.1, 0.2, 0.3 };
+        const XYZ65 dst = ColorCast<XYZ65>(ColorCast<XyY65>(src));
+
+        REQUIRE(dst.x == 0.1_nearly);
+        REQUIRE(dst.y == 0.2_nearly);
+        REQUIRE(dst.z == 0.3_nearly);
+    }
+    SECTION("XYZ50 to XyY50 to XYZ50")
+    {
+        constexpr XYZ50 src{ 0.1, 0.2, 0.3 };
+        const XYZ50 dst = ColorCast<XYZ50>(ColorCast<XyY50>(src));
+
+        REQUIRE(dst.x == 0.1_nearly);
+        REQUIRE(dst.y == 0.2_nearly);
+        REQUIRE(dst.z == 0.3_nearly);
+    }
+}
