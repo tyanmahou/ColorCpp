e0339ad888c3d92821a9ce6fda8b4215c8f173df
tyanmahou <tyanmahou@gmail.com>
2024-06-22 17:47:26 +0900

    update test

--- Tests/tests/DisplayP3.cpp
+++ Tests/tests/DisplayP3.cpp
@@ -1,5 +1,4 @@
 ﻿import ColorCpp;
-
 #include "catch.hpp"
 #include "TestUtil.hpp"
 using namespace colorcpp;
@@ -14,5 +13,33 @@ TEST_CASE("DisplayP3")
         REQUIRE(dst.x == 0.1_nearly);
         REQUIRE(dst.y == 0.2_nearly);
         REQUIRE(dst.z == 0.3_nearly);
+    }
+
+    SECTION("RGB to DisplayP3")
+    {
+        {
+            constexpr RGB src{ 0.0, 0.0, 0.0 };
+            const auto dst = ColorCast<DisplayP3>(src);
+
+            REQUIRE(dst.r == 0_nearly);
+            REQUIRE(dst.g == 0_nearly);
+            REQUIRE(dst.b == 0_nearly);
+        }
+        {
+            constexpr RGB src{ 1.0, 1.0, 1.0 };
+            const auto dst = ColorCast<DisplayP3>(src);
+
+            REQUIRE(dst.r == 1_nearly);
+            REQUIRE(dst.g == 1_nearly);
+            REQUIRE(dst.b == 1_nearly);
+        }
+        {
+            constexpr RGB src{ 0.1, 0.5, 0.9 };
+            const auto dst = ColorCast<DisplayP3>(src);
+
+            REQUIRE(dst.r == 0.046_nearly(0.01));
+            REQUIRE(dst.g == 0.207_nearly(0.01));
+            REQUIRE(dst.b == 0.732_nearly(0.01));
+        }
     }
 }
