143a23ff5756e7312222095dba00af05958f8ca2
tyanmahou <tyanmahou@gmail.com>
2024-07-28 04:32:50 +0900

    update test

--- Tests/tests/YIQ.cpp
+++ Tests/tests/YIQ.cpp
@@ -6,4 +6,13 @@ using namespace colorcpp;
 
 TEST_CASE("YIQ")
 {
+    SECTION("RGB to YIQ to RGB")
+    {
+        constexpr RGB src{ 0.1, 0.2, 0.3 };
+        const RGB dst = ColorCast< RGB>(ColorCast<YIQ>(src));
+
+        REQUIRE(dst.r == 0.1_nearly);
+        REQUIRE(dst.g == 0.2_nearly);
+        REQUIRE(dst.b == 0.3_nearly);
+    }
 }
