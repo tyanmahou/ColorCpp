f22092cc1638686700425589c7367696e3de2944
tyanmahou <tyanmahou@gmail.com>
2024-11-15 21:59:19 +0900

    update test

--- Tests/tests/ColorDiff/CIE76.cpp
+++ Tests/tests/ColorDiff/CIE76.cpp
@@ -15,7 +15,7 @@ TEST_CASE("CIE76")
         REQUIRE(diff == 173.20504_nearly(0.001));
     }
 
-    SECTION("CIE76 Diff")
+    SECTION("CIE76 RGB Diff")
     {
         constexpr Lab65 a{ 100, 0, 100 };
         constexpr Lab65 b{ 0, 100, 0 };
