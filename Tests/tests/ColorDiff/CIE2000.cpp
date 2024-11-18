f0983490a2bb6fdf5e99976b7b17891462b94504
tyanmahou <tyanmahou@gmail.com>
2024-11-18 22:55:35 +0900

    update test

--- Tests/tests/ColorDiff/CIE2000.cpp
+++ Tests/tests/ColorDiff/CIE2000.cpp
@@ -14,6 +14,15 @@ TEST_CASE("CIE2000")
         const double diff = ColorDiff::CIE2000(a, b);
         REQUIRE(diff == 100.0000_nearly(0.001));
     }
+    SECTION("CIE2000 Diff")
+    {
+        constexpr Lab65 a{ 100, 0, 100 };
+        constexpr Lab65 b{ 0, 100, 0 };
+
+        const double diff = ColorDiff::CIE2000(1, 1, 1)(a, b);
+        REQUIRE(diff == 100.0000_nearly(0.001));
+    }
+
     SECTION("CIE2000 RGB Diff")
     {
         constexpr Lab65 a{ 100, 0, 100 };
