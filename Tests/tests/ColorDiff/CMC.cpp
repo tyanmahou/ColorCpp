c57718f8dd66fae202a78bd1d45ad6f8910e9c84
tyanmahou <tyanmahou@gmail.com>
2024-11-20 22:00:48 +0900

    update test

--- Tests/tests/ColorDiff/CMC.cpp
+++ Tests/tests/ColorDiff/CMC.cpp
@@ -13,7 +13,13 @@ TEST_CASE("CMC")
 
         const double diff = ColorDiff::CMC(a, b);
         REQUIRE(diff == 102.563_nearly(0.001));
+    }
+    SECTION("CMC Diff Param")
+    {
+        constexpr Lab65 a{ 100, 0, 100 };
+        constexpr Lab65 b{ 0, 100, 0 };
 
-        REQUIRE(diff == ColorDiff::CMC(2.0, 1.0)(a, b));
+        const double diff = ColorDiff::CMC(2.0, 1.0)(a, b);
+        REQUIRE(diff == 102.563_nearly(0.001));
     }
 }
