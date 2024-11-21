6bc6da51cb14842e03d12e58dd30be32d4ca1f96
tyanmahou <tyanmahou@gmail.com>
2024-11-21 23:22:20 +0900

    update test

--- Tests/tests/ColorDiff/CIE94.cpp
+++ Tests/tests/ColorDiff/CIE94.cpp
@@ -19,8 +19,14 @@ TEST_CASE("CIE94")
         constexpr Lab65 a{ 100, 0, 100 };
         constexpr Lab65 b{ 0, 100, 0 };
 
-        const double diff = ColorDiff::CIE94(1.0, 0.045, 0.015)(a, b);
-        REQUIRE(diff == 114.891_nearly(0.001));
+        {
+            const double diff = ColorDiff::CIE94(1.0, 0.045, 0.015)(a, b);
+            REQUIRE(diff == 114.891_nearly(0.001));
+        }
+        {
+            const double diff = ColorDiff::CIE94(2.0, 0.048, 0.014)(a, b);
+            REQUIRE(diff == 77.280_nearly(0.001));
+        }
     }
     SECTION("CIE94 GraphicArts Diff")
     {
@@ -37,6 +43,5 @@ TEST_CASE("CIE94")
 
         const double diff = ColorDiff::CIE94_Textiles(a, b);
         REQUIRE(diff == 77.280_nearly(0.001));
-        REQUIRE(diff == ColorDiff::CIE94(2.0, 0.048, 0.014)(a, b));
     }
 }
