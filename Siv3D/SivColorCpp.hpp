357933316c0c9956ffb061c13d211c6975bd5176
tyanmahou <tyanmahou@gmail.com>
2024-11-26 23:02:11 +0900

    update nodiscard

--- Siv3D/SivColorCpp.hpp
+++ Siv3D/SivColorCpp.hpp
@@ -13,7 +13,7 @@ namespace colorcpp
     template<>
     struct ColorCastTraits<s3d::ColorF, RGBA>
     {
-        constexpr static s3d::ColorF Cast(const RGBA& from)
+        [[nodiscard]] constexpr static s3d::ColorF Cast(const RGBA& from)
         {
             auto&& [r, g, b, a] = from;
             return { r, g, b, a };
@@ -22,7 +22,7 @@ namespace colorcpp
     template<>
     struct ColorCastTraits<RGBA, s3d::ColorF>
     {
-        constexpr static RGBA Cast(const s3d::ColorF& from)
+        [[nodiscard]] constexpr static RGBA Cast(const s3d::ColorF& from)
         {
             auto&& [r, g, b, a] = from;
             return { r, g, b, a };
@@ -33,7 +33,7 @@ namespace colorcpp
     template<>
     struct ColorCastTraits<s3d::Color, RGBA32>
     {
-        constexpr static s3d::Color Cast(const RGBA32& from)
+        [[nodiscard]] constexpr static s3d::Color Cast(const RGBA32& from)
         {
             auto&& [r, g, b, a] = from;
             return { r, g, b, a };
@@ -42,7 +42,7 @@ namespace colorcpp
     template<>
     struct ColorCastTraits<RGBA32, s3d::Color>
     {
-        constexpr static RGBA32 Cast(const s3d::Color& from)
+        [[nodiscard]] constexpr static RGBA32 Cast(const s3d::Color& from)
         {
             auto&& [r, g, b, a] = from;
             return { r, g, b, a };
@@ -58,7 +58,7 @@ namespace colorcpp
     template<>
     struct ColorCastTraits<s3d::HSV, colorcpp::HSV>
     {
-        constexpr static s3d::HSV Cast(const colorcpp::HSV& from)
+        [[nodiscard]] constexpr static s3d::HSV Cast(const colorcpp::HSV& from)
         {
             return { from.h, from.s, from.v };
         }
@@ -66,7 +66,7 @@ namespace colorcpp
     template<>
     struct ColorCastTraits<colorcpp::HSV, s3d::HSV>
     {
-        constexpr static colorcpp::HSV Cast(const s3d::HSV& from)
+        [[nodiscard]] constexpr static colorcpp::HSV Cast(const s3d::HSV& from)
         {
             return { from.h, from.s, from.v };
         }
